5f25fd6551e7167455cb018d7c1fb2e76d944022
StephanOrgiazzi <stephan.orgiazzi@gmail.com>
2025-01-03 23:46:09 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -6,10 +6,7 @@ const nextConfig = {
   swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
-  },
-  experimental: {
-    optimizeCss: true,
-  },
+  }
 }
 
 module.exports = nextConfig
