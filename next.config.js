0ef2b29c0c2d20d4d62088e0cdf5a9f0ce0cf415
StephanOrgiazzi <stephan.orgiazzi@gmail.com>
2025-01-03 23:44:23 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,8 +69,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className={`${inter.className} h-full`}>
-        {children}
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={`${inter.className} h-full antialiased bg-gradient-to-b from-teal-950 via-black to-black`}>
+        <main className="h-full flex items-center justify-center">
+          {children}
+        </main>
       </body>
     </html>
   )
