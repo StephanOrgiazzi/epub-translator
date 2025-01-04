00aff8f52938cecb87cf935c0e539cf5f2e20ce3
StephanOrgiazzi <stephan.orgiazzi@gmail.com>
2025-01-04 01:28:45 +0100

    update wording

--- src/components/StaticContent.tsx
+++ src/components/StaticContent.tsx
@@ -4,20 +4,14 @@ export function StaticContent() {
   return (
     <>
       {/* SEO-friendly static content */}
-      <h1 className="text-2xl sm:text-3xl font-bold text-white mb-2">
+      <h1 className="text-2xl font-bold text-white mb-2">
         EPUB Book Translator
       </h1>
       <div className="prose prose-invert max-w-none mb-4">
-        <p className="text-gray-300 text-xs sm:text-base">
-          Instantly translate your EPUB books to many languages for FREE!
+        <p className="text-gray-300 text-sm mb-2">
+          Instantly translate your EPUB ebooks to many languages for FREE.
         </p>
-        <div className="mt-4 space-y-1">
-          <h2 className="text-md sm:text-lg font-semibold text-white">Features:</h2>
-          <ul className="list-disc pl-4 text-gray-300 text-xs sm:text-sm">
-            <li className='mb-2'>Support for languages including French, Spanish, German, Italian, and many more</li>
-            <li>Preserve original EPUB formatting and structure</li>
-          </ul>
-        </div>
+        <p className="text-gray-300 text-sm hidden sm:block">Support for languages including Spanish, German, Portuguese, French, Italian, Swedish, and many more!</p>
       </div>
     </>
   );
